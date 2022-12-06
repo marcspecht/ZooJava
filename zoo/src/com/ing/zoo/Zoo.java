8867a63f4dfce12583049c08d1580a7fbdac969e
Marc Specht <marc.specht@hva.nl>
2022-12-06 21:44:11 +0100

    added all command support

--- zoo/src/com/ing/zoo/Zoo.java
+++ zoo/src/com/ing/zoo/Zoo.java
@@ -3,6 +3,7 @@ package com.ing.zoo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Zoo {
@@ -18,8 +19,7 @@ public class Zoo {
 
         Lion henk = new Lion();
         henk.name = "henk";
-        animals.add(henk);
-
+        
         Hippo elsa = new Hippo();
         elsa.name = "elsa";
         Pig dora = new Pig();
@@ -29,36 +29,70 @@ public class Zoo {
         Zebra marty = new Zebra();
         marty.name = "marty";
 
-        while(true){
-            Scanner scanner = new Scanner(System.in);
-            
-            System.out.print("Voer uw command in: ");
+        animals.add(henk);
+        animals.add(elsa);
+        animals.add(dora);
+        animals.add(wally);
+        animals.add(marty);
 
-            String input = scanner.nextLine();
-            String[] inputArray = input.split(" ", 2);
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Voer uw command in: ");
 
-            // Check if command is valid
-            boolean inputIsInList = Arrays.asList(commands).contains(inputArray[0]);
-            if(!inputIsInList){
-                System.out.println("Unknown command: " + inputArray[0]);
-                continue;
-            }
+        String input = scanner.nextLine();
+        String[] inputArray = input.split(" ", 3);
 
-            // Check if animal exists
-            Animal foundAnimal = animals.stream().filter(x -> x.toString() == inputArray[1]).findFirst().get();
-            if(foundAnimal == null){
-                System.out.println("Animal not found: " + inputArray[1]);
-                continue;
-            }    
-
-
-
-            if(input.equals(commands[0] + " henk"))
-            {
-                henk.sayHello();
-            }
-            
-            scanner.close();
+        // Check if command is valid
+        var command = inputArray[0];
+        if(inputArray.length > 2)
+            command = inputArray[0] + " " +inputArray[1];
+        
+        boolean inputIsInList = Arrays.asList(commands).contains(command);
+        if(!inputIsInList){
+            System.out.println("Unknown command: " + command);
+            System.exit(0);
         }
+
+        // Check if animal exists
+        var name = inputArray[inputArray.length - 1];
+        Optional<Animal> optionalAnimal = animals.stream().filter(x -> name.equals(x.toString())).findFirst();
+        if(!optionalAnimal.isPresent()){
+            System.out.println("Animal not found: " + inputArray[inputArray.length - 1]);
+            System.exit(0);
+        }    
+
+        Animal animal = optionalAnimal.get();
+
+        switch(command){
+            case "hello":
+                animal.sayHello();
+                break;
+            case "give leaves":
+                try{
+                    animal.eatLeaves();
+                } catch(UnsupportedOperationException e){
+                    System.out.println("Animal does not eat leaves");
+                }
+                break;
+            case "give meat":
+                try{
+                    animal.eatMeat();
+                } catch(UnsupportedOperationException e){
+                    System.out.println("Animal does not eat meat");
+                }
+                break;
+            case "perform trick":
+                try{
+                    animal.performTrick();
+                } catch(UnsupportedOperationException e){
+                    System.out.println("Animal does not now any tricks");
+                }
+                break;
+            default:
+                break;
+        }
+
+        scanner.close();
     }
 }
